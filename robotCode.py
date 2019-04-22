7de248dc6fd94e14648d37a855df005a5f712fbe
Dival Banerjee <bandiv2011@gmail.com>
2019-04-22 03:49:00 -0400

    Working on robot code

--- robotCode.py
+++ robotCode.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python3
-from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_D, OUTPUT_B,SpeedPercent, MoveTank
+from ev3dev2.motor import LargeMotor, MediumMotor,OUTPUT_A, OUTPUT_D, OUTPUT_B,SpeedPercent, MoveTank
 from ev3dev2.sensor import INPUT_1
 from ev3dev2.sensor.lego import TouchSensor
 from ev3dev2.led import Leds
+from time import sleep
 
 from ev3dev2.sound import Sound
 
 sound = Sound()
-sound.speak('ENED Bad')
+#ound.speak('ENED Bad')
 
 motorArm = MediumMotor(OUTPUT_B)
 
-motorArm.
+
+angle = 220/0.142857
+motorArm.on_for_degrees(-50,angle)
+sleep(5)
+motorArm.on_for_degrees(50,angle)
 
 #tank_drive = MoveTank(OUTPUT_A, OUTPUT_D)
 
@@ -21,4 +26,4 @@ motorArm.
 
 # drive in a different turn for 3 seconds
 
-tank_drive.on_for_degrees(SpeedPercent(-30), SpeedPercent(30), 90)
+#tank_drive.on_for_degrees(SpeedPercent(-30), SpeedPercent(30), 90)
--- table.py
+++ table.py
@@ -11,41 +11,91 @@ class RobotGripper:
         self.angleToRunTo = 0
         self.motorB = MediumMotor(OUTPUT_B)
         self.CurrentAngle = 0
+        self.motorAngle = 0
+        self.motorDestinationAngle = 0
         self.Hold = False
-    
+           
+    def findMotorAngle(self):
+        motorArm.on_for_degrees(-50,angle)
+
+    def extend(self):
+        self.motorDestinationAngle = (angleToRunTo-CurrentAngle)/0.142857
+        self.motorArm.on_for_degrees(-50,angle)
+
     def setHold(status):
-        self.
+        self.motorB.on_for_degrees(100,0,brake = status, block = True)
+    
+        
 
+class IGPS:
+    def __init__(self):
+        self.radiusA = 0
+        self.radiusB = 0
+        self.radiusC = 0
 
+class myRobot:
+    def __init__(self):
+        self.tankDrive = MoveTank(OUTPUT_A,OUTPUT_D)
+        self.motorA = LargeMotor(OUTPUT_A)
+        self.motorD = LargeMotor(OUTPUT_D)
+        self.gyro = GyroSensor()
+        self.myGripper = RobotGripper()
+        self.Turning = False
+        self.Moving = False
+        self.Angle = False
+        self.positionX = 0
+        self.positionY = 0
+        self.myDefaultSpeed = 50;
+    def getAverageDistance(self):
+        average = (motorA.position + motorD.position)/2
+        return average
 
+    def moveStraight(self,distance):
+        motorA.reset()
+        motorD.reset()
+        tankDrive.reset()
+        GyroSensor.reset()
+        currentDistance = 0
+        while currentDistance < distance:
+            currentDistance = getAverageDistance()
+            tankDrive.on_for_degrees(SpeedPercent(),SpeedPercent(), 10)
+            #movesteer('rotations',steer = , pwr=50, rots=.1)
+        #Set to brake
 
-tankDrive = MoveTank(OUTPUT_A,OUTPUT_D)
-motorA = LargeMotor(OUTPUT_A)
-motorD = LargeMotor(OUTPUT_D)
-gyro = GyroSensor()
+    def collisonDistance(self):
+        distance = UltrasonicSensor.distance_inches_ping
+        return distance
 
-def getAverageDistance():
-    average = (motorA.position + motorD.position)/2
-    return average
+    def collisionAvoidance(self):
+        if self.Turning == False :
+            #Normal collision avoidance
+        else:
+            #Collision avoidance disabled
+            print('Not checking for collisions')
 
-def moveStraight(distance):
-    motorA.reset()
-    motorD.reset()
-    tankDrive.reset()
-    GyroSensor.reset()
-    currentDistance = 0
-    while currentDistance < distance:
-        currentDistance = getAverageDistance()
-        tankDrive.on_for_degrees(SpeedPercent(),SpeedPercent(), 10)
-        #movesteer('rotations',steer = , pwr=50, rots=.1)
-    #Set to brake
+    def turnToAngle(self,angle):
+        if angle > 0:
+            print('Positive angle')
+            #Do something
+            
+        else:
+            #do another thing
+            print ('Negative Angle')
 
-def collisonDistance():
-    distance = UltrasonicSensor.distance_inches_ping
-    return distance
+    def readBarCode(self):
+        #String kmessage is 3 letters, 'W' denotates white, "B" denotates black
+        output = ""
+        
+        return output
 
-def turnToAngle(angle):
-    if angle > 0:
-        #Do something
-    else:
-        #do another thing
+class RobotRunner
+    def __init__(self):
+        self.myBot = myRobot
+    def task1(self):
+        #Do task 1
+    def task2(self):
+    
+    def mainDemo(self):
+
+ #main 
+
